a20ca0e69d5ab0a21b83131c634a97f83f750675
Saudagar Dalve <111639365+saudagar002@users.noreply.github.com>
2023-03-09 15:38:03 +0530

    Add files via upload

---
+++ Event.github.io-main/Event.github.io-main/index.js
@@ -0,0 +1,25 @@
+// const EventEmmiter=require('events')
+// const event = new EventEmmiter(); // object Creation
+
+
+// //event defination
+
+// event.on("Click",()=>{
+
+//     console.log("Event is Fire")
+// })
+
+// event.emit("Click") //event call
+      
+ 
+
+// const EventEmmiter = require('events')
+
+// const event = new EventEmmiter();
+
+// event.on('submit',()=>{
+//     console.log('Event is Completed')
+
+// })
+
+// event.emit('submit')
